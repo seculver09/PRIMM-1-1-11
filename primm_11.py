6682e1d363cab6aa4591acf44f72f53b2b29b259
seculver09 <seculver09@gmail.com>
2024-10-02 09:51:41 -0600

    Update primm_11.py to complete code.

--- primm_11.py
+++ primm_11.py
@@ -5,12 +5,17 @@ Name - Date
 """
 
 def main():
-  
-    num1: int = input("Enter a number: ")
-    num2: int = input("Enter another number: ")
-    total: int = num1+num2
+  #this major chunk of code just says combine two numbers together ex. 8+9=89
+    num1: float = float(input("Enter a number: "))
+    num2: float = float(input("Enter another number: "))
+    total: float = num1+num2
 
     print(f"{num1} + {num2} = {total}")
+    print(f"{num1} - {num2} = {num1-num2}")
+    print(f"{num1} * {num2} = {num1*num2}")
+    print(f"{num1} / {num2} = {num1/num2}")
+    print(f"{num1} // {num2} = {num1//num2}")
+    print(f"{num1} % {num2} = {num1%num2}")
 
 if __name__ == "__main__":
   main()
